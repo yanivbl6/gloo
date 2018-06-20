5952a32ec7e7af877c3f329fc195b764c12b2855
Yaniv Blumenfeld <yanivbl@mellanox.com>
2018-06-20 13:39:02 +0300

    Fixed ring-allreduce logic- stable

--- gloo/benchmark/runner.cc
+++ gloo/benchmark/runner.cc
@@ -203,7 +203,7 @@ void Runner::run(BenchmarkFn<T>& fn) {
   }
 
   // Run sweep over number of elements
-  for (int i = 16; i <= 1 << 14 ; i *= 4) {
+  for (int i = 16; i <= 1 << 14 ; i *= 2) {
     //std::vector<int> js = {i * 1, i * 2, i * 5};
     run(fn, i);
   }
